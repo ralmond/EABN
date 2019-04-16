3158f323a8d32eed23e32540823e77c437a16c36
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-04-16 21:06:20 +0000

    Proc4Dep

--- R/Evidence.R
+++ R/Evidence.R
@@ -2,26 +2,8 @@
 ###  This roughy follow a combination of the xAPI format.
 
 setClass("EvidenceSet",
-         slots=c("_id"="character",    #Mongo ID
-                 app="character",       #Application ID
-                 uid="character",       #User (student) ID
-                 context="character",      #If context is calculated by PP
-                 mess="character",      #Action Identifier
-                 timestamp="POSIXt",      #When action took place.
-                 obs="list",              #More details.
-                 seqno="integer"))
-setGeneric("app",function(x) standardGeneric("app"))
-setMethod("app","EvidenceSet", function(x) x@app)
-setGeneric("uid",function(x) standardGeneric("uid"))
-setMethod("uid","EvidenceSet", function(x) x@uid)
-setGeneric("mess",function(x) standardGeneric("mess"))
-setMethod("mess","EvidenceSet", function(x) x@mess)
-setGeneric("context",function(x) standardGeneric("context"))
-setMethod("context","EvidenceSet", function(x) x@context)
-setGeneric("timestamp",function(x) standardGeneric("timestamp"))
-setMethod("timestamp","EvidenceSet", function(x) x@timestamp)
-setGeneric("details",function(x) standardGeneric("details"))
-setMethod("details","EvidenceSet", function(x) x@obs)
+         slots=c(seqno="integer"),
+         contains="P4Message")
 
 EvidenceSet <- function(uid,context,timestamp=Sys.time(),
                         obs=list(),app="default",mess="Accumulate") {
@@ -29,6 +11,11 @@ EvidenceSet <- function(uid,context,timestamp=Sys.time(),
       timestamp=timestamp,obs=obs,"_id"=NA_character_,
       seqno=NA_integer_ )
 }
+
+setGeneric("seqno",function(x) standardGeneric("seqno"))
+setMethod("seqno","Event", function(x) x@seqno)
+
+
 
 setMethod("toString","EvidenceSet", function(x, ...) {
   paste('EvidenceSet:{ uid:',x@uid,', context:',x@context,
@@ -38,176 +25,30 @@ setMethod("show","EvidenceSet",function(object) {
   cat(toString(object),"\n")
 })
 
-
-
-
-setGeneric("as.json",function(x) standardGeneric("as.json"))
-setMethod("as.json","EvidenceSet", function(x) {
-  esl <- attributes(x)
-  esl$"_id" <- NULL
-  if (is.na(esl$seqno)) esl$seqno <- NULL
-  esl$class <-NULL
-  esl$timestamp <- unbox(esl$timestamp) # Auto_unbox bug.
-  raw <- toJSON(esl,auto_unbox=TRUE,POSIXt="mongo")
-  ## Timestamp is not unboxed.  Need to do that manually.
-  ## sub('"timestamp":\\[(.*)\\]','"timestamp":\\1',raw)
-  raw
+setMethod("as.jlist",c("EvidenceSet","list"), function(obj,ml,serialize=TRUE) {
+  ## Call Next Method
+  as.p4jlist <- getMethod("as.jlist",c("P4Message","list"))
+  ml <- do.call(as.p4jlist,list(obj,ml,serialize))
+  ## Additional work
+  if (is.na(ml$seqno)) {
+    ml$seqno <- NULL
+  } else {
+    ml$seqno <- unbox(ml$seqno)
+  ml
   })
 
-dquote <- function (str) {paste('"',str,'"',sep="")}
-
-saveES <- function (es, col) {
-  if (is.na(es@"_id")) {
-    ## Insert
-    jso <- as.json(es)
-    col$insert(jso)
-    it <- col$iterate(jso,'{"_id":true}',limit=1)
-    es@"_id" <- it$one()$"_id"
-  } else {
-    ## Replace
-    col$update(paste('{"_id":{"$oid":"',es@"_id",'"}}',sep=""),
-               paste('{"$set":',as.json(es),'}',sep=""))
-  }
-  es
-}
-
-
-getESbyID <- function(id,col) {
-  it <- col$iterate(paste('{"_id":{"$oid":"',id,'"}}',sep=""),
-                    '{}',limit=1)
-  rec <- it$one()
-  if (is.null(rec)) return(rec)
-  parseEvidence(rec)
-}
-
-
-
 parseEvidence<- function (rec) {
-  new("EvidenceSet","_id"=rec$"_id", app=rec$app, uid=rec$uid,
-      context=rec$context,mess=rec$mess,
-      timestamp=rec$timestamp,
-      obs=parseObs(rec$obs),
-      seqno=rec$seqno)
+  if (is.null(rec$"_id")) rec$"_id" <- NA_character_
+  names(rec$"_id") <- "oid"
+  if (is.null(rec$app)) rec$app <- "default"
+  if (is.null(rec$seqno)) rec$seqno <- NA_integer_
+  if (is.null(rec$timestamp)) rec$timestamp <- Sys.time()
+  new("EvidenceSet","_id"=rec$"_id", app=as.vector(rec$app),
+      uid=as.vector(rec$uid),
+      context=as.vector(rec$context),mess=as.vector(rec$mess),
+      timestamp=as.POSIXlt(ununboxer(rec$timestamp)),
+      data=parseData(ununboxer(rec$data)),
+      seqno=as.vector(rec$seqno))
 }
 
-parseObs <- function (obsObj) {
-  ##Not sure if we need further processing.
-  obsObj
-}
-
-buildESquery <- function (uid,context,mess,before,after,
-                          timestamp=NULL,seqno=NA_integer_,
-                          app="default") {
-  query <- '{'
-  ## app
-  if (length(app)> 1L) {
-    app <- paste('{"$in":',toJSON(app),'}',sep="")
-  }
-  query <- paste(query,'"app":"',app,'"',sep="")
-  ## uid
-  if (missing(uid)) {
-    stop("Must specify user/student id.")
-  }
-  if (length(uid) > 1L) {
-    uid <- paste('{"$in":',toJSON(uid),'}',sep="")
-  } else {
-    uid <- toJSON(uid,auto_unbox=TRUE)
-  }
-  query <- paste(query,', "uid":',uid,sep="")
-  ## Context
-  if (!missing(context)) {
-    if (length(context) > 1L) {
-      context <- paste('{"$in":',toJSON(context),'}',sep="")
-    } else {
-      context <- toJSON(context,auto_unbox=TRUE)
-    }
-    query <- paste(query,', "context":',context,sep="")
-  }
-  ## Mess(age)
-  if (!missing(mess)) {
-    if (length(mess) > 1L) {
-      mess <- paste('{"$in":',toJSON(mess),'}',sep="")
-    } else {
-      mess <- toJSON(mess,auto_unbox=TRUE)
-    }
-    query <- paste(query,', "mess":',mess,sep="")
-  }
-  ## Timestamp -- can be either single time or before or after range.
-  if (!missing(timestamp)) {
-    timestamp <- paste('"$in:"',toJSON(timestamp,POSIXt="mongo"))
-  } else {
-    lt <- NULL
-    if (!missing(before) && is(before,"POSIXt")) {
-      if (length(before) > 1L) {
-        stop("Before must be a single POSIXt or integer.")
-      }
-      ## Need to strip array marks off
-      lt <- paste('"$lt":',toJSON(unbox(before),POSIXt="mongo"))
-    }
-    gt <- NULL
-    if (!missing(after) && is(after,"POSIXt")) {
-      if (length(after) > 1L) {
-        stop("After must be a single POSIXt or integer.")
-      }
-      ## Need to strip array marks off
-      gt <- paste('"$gt":',toJSON(unbox(after),POSIXt="mongo"))
-    }
-    timestamp <- paste(c(lt,gt),collapse=",")
-  }
-  if (nchar(timestamp) > 0L) {
-    query <- paste(query,', "timestamp":{',timestamp,'}',sep="")
-  }
-
-  ## seqno -- can be either single number or before and after range.
-  if (!missing(seqno)) {
-    ## This produces sloppy code when seqno is a single number, but
-    ## requires fewer branches.
-    seqno <- paste('"$in:"',toJSON(seqno))
-  } else {
-    lt <- NULL
-    if (!missing(before) && is.numeric(before)) {
-      if (length(before) > 1L) {
-        stop("Before must be a single POSIXt or integer.")
-      }
-      ## Need to strip array marks off
-      lt <- paste('"$lt":',toJSON(unbox(before)))
-    }
-    gt <- NULL
-    if (!missing(after) && is.numeric(after)) {
-      if (length(after) > 1L) {
-        stop("After must be a single POSIXt or integer.")
-      }
-      ## Need to strip array marks off
-      gt <- paste('"$gt":',toJSON(unbox(after)))
-    }
-    seqno <- paste(c(lt,gt),collapse=",")
-  }
-  if (nchar(seqno) > 0L) {
-    query <- paste(query,', "seqno":{',seqno,'}',sep="")
-  }
-
-  #### Return as string.
-  paste(query,'}',sep="")
-}
-
-
-getESone <- function(query,col) {
-  it <- col$iterate(query,'{}',sort='{"timestamp":-1}',limit=1)
-  rec <- it$one()
-  if (is.null(rec)) return(rec)
-  parseEvidence(rec)
-}
-
-getESmany <- function(query,sort=-1,col) {
-  n <- col$count(query)
-  result <- vector("list",n)
-  it <- col$iterate(query,'{}',
-                    sort=paste('{"timestamp":',sort,'}',sep=""))
-  nn <- 1
-  while (!is.null(rec <- it$one())) {
-    result[[nn]] <- parseEvidence(rec)
-    nn <- nn +1
-  }
-  result
-}
 
--- R/StudentRec.R
+++ R/StudentRec.R
@@ -17,12 +17,15 @@ setClass("StudentRecord",
                  stats="list",
                  seqno="integer",
                  prev="ANY",prev_id="character"))        #More details.
+
 setMethod("app","StudentRecord", function(x) x@app)
 setMethod("uid","StudentRecord", function(x) x@uid)
 setMethod("context","StudentRecord", function(x) x@context)
 setMethod("timestamp","StudentRecord", function(x) x@timestamp)
+
 setGeneric("sm",function(x) standardGeneric("sm"))
 setMethod("sm","StudentRecord", function(x) x@sm)
+
 setGeneric("stats",function(x) standardGeneric("stats"))
 setMethod("stats","StudentRecord", function(x) x@stats)
 
@@ -36,6 +39,9 @@ StudentRecord <- function(uid,context="",timestamp=Sys.time(),
       prev_id=NA_character_)
 }
 
+setGeneric("evidence",function(x) standardGeneric("evidence"))
+setMethod("evidence","StudentRecord", function(x) x@evidence)
+
 setMethod("toString","StudentRecord", function(x, ...) {
   paste('StudentRecord:{uid:',x@uid,', context:',x@context,
         ', seqno:',x@seqno,'}')
@@ -44,53 +50,66 @@ setMethod("show","StudentRecord",function(object) {
   cat(toString(object),"\n")
 })
 
-setMethod("as.json","StudentRecord", function(x) {
-  srl <- attributes(x)
-  srl$"_id" <- NULL
-  if (is.na(srl$seqno)) srl$seqno <- NULL
-  srl$class <-NULL
+setMethod("as.jlist",c("StudentRecord","list"), function(obj,ml,serialize=TRUE) {
+  ml$"_id" <- NULL
+  ml$class <- NULL
+  ml$app <- unboxer(ml$app)
+  ml$uid <- unboxer(ml$uid)
+  if (!is.null(ml$context) && length(ml$context)==1L)
+    ml$context <- unboxer(ml$context)
+  ml$timestamp <- unboxer(ml$timestamp) # Auto_unbox bug.
+
   ## Serialize SM if necessary.
   if (!is.null(x@sm)) {
     smo <- PnetSerialize(x@sm)
     smo$data <- base64_enc(smo$data)
-    srl$sm <- smo
+    ml$sm <- smo
   } else {
     srl$sm <- NULL
   }
   ## Normalize Evidence Sets
-  srl$evidence <- NULL
-  srl$"evidence_id" <- NULL
+  ml$evidence <- NULL
+  ml$"evidence_id" <- NULL
   if (length(x@"evidence_id")>0L) {
-    srl$evidence <- x@"evidence_id"
+    ml$evidence <- unboxer(x@"evidence_id")
   } else if (length(x@evidence) > 0L) {
-    srl$evidence <- sapply(x@evidence, function (e) e@"_id")
+    ml$evidence <- sapply(x@evidence, function (e) e@"_id")
   }
   ## Normalize Previod
-  srl$prev <- NULL
-  srl$"prev_id" <- NULL
+  ml$prev <- NULL
+  ml$"prev_id" <- NULL
   if (!is.na(x@"prev_id")) {
-    srl$prev <- x@"prev_id"
+    ml$prev <- unboxer(x@"prev_id")
   } else if (!is.null(x@prev)) {
-    srl$prev <- x@prev@"_id"
+    ml$prev <- unboxer(x@prev@"_id")
   }
-  ## Fix for auto_unbox bug
-  ## Turns out I want finer control on the auto-unbox, so will use
-  ## manual unboxes.
-  srl$app <- unbox(srl$app)
-  srl$uid <- unbox(srl$uid)
-  srl$context <- unbox(srl$context)
-  srl$timestamp <- unbox(srl$timestamp)
-  srl$seqno <- unbox(srl$seqno)
-  srl$prev <- unbox(srl$prev)
-  srl$sm <- lapply(srl$sm,unbox)
-  ## Saves name data
-  srl$stats <- lapply(srl$stats,
-                      function (s) lapply(s,unbox)) #Saves name data.
-  ## Now serialize
-  toJSON(srl,POSIXt="mongo")
-  })
+  ml
+}
 
+parseStudentRecord <- function (rec) {
+  if (is.null(rec$"_id")) rec$"_id" <- NA_character_
+  names(rec$"_id") <- "oid"
+  if (is.null(rec$evidence_id)) rec$evidence_id <- NA_character_
+  if (is.null(rec$prev_id)) rec$prev_id <- NA_character_
+  if (is.null(rec$seqno)) rec$seqno <- NA_integer_
+  elist <- list()
+  smo <- NULL  ## HERE
+  slist <- list()
+  prev <- NULL
+  new("StudentRecord","_id"=ununboxer(rec$"_id"),
+      app=ununboxer(rec$app), context=ununboxer(rec$context),
+      uid=ununboxer(rec$uid),
+      timestamp=as.POSIXlt(ununboxer(rec$timestamp)),
+      evidence_id=ununboxer(rec$evidence_id),evidence=elist,
+      sm=smo,stats=slist,prev=prev,prev_id=ununboxer(rec$prev_id))
+  }
 
+<<<<<<< .mine
+
+||||||| .r809
+setGeneric("evidence",function(x) standardGeneric("evidence"))
+setMethod("evidence","StudentRecord", function(x) x@evidence)
+=======
 parseSR<- function (rec) {
   smo <- rec$sm
   if (!is.null(smo)) {
@@ -269,3 +288,4 @@ setGeneric("evidence",function(x) standardGeneric("evidence"))
 setMethod("evidence","StudentRecord", function(x) x@evidence)
 
 
+>>>>>>> .r985
