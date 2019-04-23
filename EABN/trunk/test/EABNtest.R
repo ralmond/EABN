95230cb9ecb9c8cda526e9fc90d9b36104154112
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-04-23 03:08:24 +0000

    ManifestlyTested

--- EABN/trunk/test/EABNtest.R
+++ EABN/trunk/test/EABNtest.R
@@ -1,11 +1,22 @@
+library(EABN)
+flog.threshold(DEBUG)
 
 testdir <- "/home/ralmond/Projects/EABN/inst"
 mantab <- read.csv(file.path(testdir,"sampleAssessment",
                              "PPsubsetManifest.csv"),as.is=TRUE)
 rownames(mantab) <- mantab$Name
 
-app <- "ecd://epls.coe.fsu.edu/P4test"
-col <- mongo("manfred")
-mantab$app <- "ecd://epls.coe.fsu.edu/P4test"
-col$insert(mantab)
-col$find(sprintf('{ "app":"%s"}',app))
+stattab <- read.csv(file.path(testdir,"sampleAssessment",
+                             "StatisticList.csv"),as.is=TRUE)
+
+tapp <- "ecd://epls.coe.fsu.edu/P4test"
+
+source("~/.Netica.R")
+sess <- NeticaSession(LicenseKey=NeticaLicenseKey)
+startSession(sess)
+
+cl <- new("CaptureListener")
+
+EAeng <- BNEngine(tapp,sess,list(cl))
+loadManifest(EAeng,mantab)
+configStats(EAeng,stattab)
