8ba9659ffcca20c33418e1fd19b4c02014824dbe
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-04-25 01:16:58 +0000

    Default SR

--- test/EABNtest.R
+++ test/EABNtest.R
@@ -5,6 +5,9 @@ testdir <- "/home/ralmond/Projects/EABN/inst"
 mantab <- read.csv(file.path(testdir,"sampleAssessment",
                              "PPsubsetManifest.csv"),as.is=TRUE)
 rownames(mantab) <- mantab$Name
+mantab$Pathname <- file.path(testdir,"sampleAssessment",mantab$Pathname)
+
+
 
 stattab <- read.csv(file.path(testdir,"sampleAssessment",
                              "StatisticList.csv"),as.is=TRUE)
@@ -18,5 +21,10 @@ startSession(sess)
 cl <- new("CaptureListener")
 
 EAeng <- BNEngine(tapp,sess,list(cl))
+
 loadManifest(EAeng,mantab)
 configStats(EAeng,stattab)
+
+setupDefaultSR(EAeng)
+
+dsr1<- EAeng$studentRecords()$defaultSR
