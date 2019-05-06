bd2135ff04f886c4d09488dd05ca1467a5e5d068
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2019-05-06 19:31:21 +0000

    Save/RestoreSR

--- test/EABNtest.R
+++ test/EABNtest.R
@@ -1,41 +1,62 @@
 library(EABN)
 flog.threshold(DEBUG)
 
-testdir <- "/home/ralmond/Projects/EABN/inst"
-mantab <- read.csv(file.path(testdir,"sampleAssessment",
-                             "PPsubsetManifest.csv"),as.is=TRUE)
-rownames(mantab) <- mantab$Name
-mantab$Pathname <- file.path(testdir,"sampleAssessment",mantab$Pathname)
-
-
-
-stattab <- read.csv(file.path(testdir,"sampleAssessment",
-                             "StatisticList.csv"),as.is=TRUE)
 
 tapp <- "ecd://epls.coe.fsu.edu/P4test"
 
-source("~/.Netica.R")
+source("~/Netica.R")
 sess <- NeticaSession(LicenseKey=NeticaLicenseKey)
 startSession(sess)
 
 cl <- new("CaptureListener")
 ul <- UpdateListener(dbname="Proc4",dburi="mongodb://localhost",
             colname="Statistics",targetField="data",
-            messSet=c("Statistics")
-            jsonEncoder="stats2json"))
+            messSet=c("Statistics"),
+            jsonEncoder="stats2json")
 
 
 
-EAeng <- BNEngine(tapp,sess,list(cl,ul))
+EAeng <- BNEngine(tapp,sess,list(cl=cl,ul=ul))
 loadManifest(EAeng)
 configStats(EAeng)
 
-loadManifest(EAeng,mantab)
-configStats(EAeng,stattab)
+### One time setup
+if (false) {
+  testdir <- "/home/ralmond/Projects/EABN/inst"
+
+  mantab <- read.csv(file.path(testdir,"sampleAssessment",
+                               "PPsubsetManifest.csv"),as.is=TRUE)
+  rownames(mantab) <- mantab$Name
+  mantab$Pathname <- file.path(testdir,"sampleAssessment",mantab$Pathname)
+
+
+
+  stattab <- read.csv(file.path(testdir,"sampleAssessment",
+                                "StatisticList.csv"),as.is=TRUE)
+  loadManifest(EAeng,mantab)
+  configStats(EAeng,stattab)
+
+  ## Configure Hist Nodes.
+  hnodes <- c("Physics","Torque","Energy","LinearMomentum","ForceAndMotion")
+  hnodes <- c("Physics")
+  EAeng$histNodesdb()$insert(sprintf('{"app":"%s","Nodes":[%s]}',
+                                     app(EAeng),
+                                     paste(sapply(hnodes,function(x) sprintf('"%s"',x)),
+                                           collapse=", ")))
+
+}
 
 setupDefaultSR(EAeng)
 
 srs<- EAeng$studentRecords()
 
 fsr <- getSR(srs,"Phred")
+fsr0 <- getSR(srs,"Phred")
 
+es1 <- EvidenceSet(uid="Phred",context="ArountTheTree",
+                   obs=list(Duration=39.5,Agent="Ramp",
+                            NumberAttempts=2,
+                            TrophyLevel="Gold"),
+                   app=app(EAeng),mess="Observables")
+
+es1 <- logEvidence(EAeng,fsr,es1)
