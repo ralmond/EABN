6f813ce3322d58b06bfc7253942068410689ba59
Russell Almond <almond@acm.org>
2021-08-14 17:08:05 -0400

    Changing scripts.

--- inst/conf/EABuild.R
+++ inst/conf/EABuild.R
@@ -2,17 +2,33 @@ library(R.utils)
 library(EABN)
 library(PNetica)
 
+if (interactive()) {
+  ## Edit these for the local application
+  appStem <- "P4test"
+  loglevel <- ""
+  override <- FALSE
+} else {
+  appStem <- cmdArg("app",NULL)
+  if (is.null(app) || !grepl("^ecd://",app))
+    stop("No app specified, use '--args app=ecd://...'")
+  loglevel <- cmdArg("level","")
+  override <- as.logical(cmdArg("override",FALSE))
+}
+
 source("/usr/local/share/Proc4/EAini.R")
 
 EA.config <- fromJSON(file.path(config.dir,"config.json"),FALSE)
 
 logfile <- (file.path(logpath, sub("<app>","Builder",EA.config$logname)))
+## Let command line override configuration.
+if (nchar(loglevel)==OL) loglevel <- EA.config$logLevel
+
 if (interactive()) {
   flog.appender(appender.tee(logfile))
 } else {
   flog.appender(appender.file(logfile))
 }
-flog.threshold(EA.config$loglevel)
+flog.threshold(loglevel)
 
 appStem <- as.character(EA.config$appStem)
 
@@ -26,6 +42,15 @@ if (length(apps)==0L || any(apps=="NULL")) {
 sess <- NeticaSession(LicenseKey=NeticaLicenseKey)
 startSession(sess)
 
-## <<HERE>> Need to load extensions.
+## Load extensions.
+for (ext in EA.config$extensions) {
+  if (is.character(ext) && nchar(ext) > 0L) {
+    if (file.exists(file.path(config.dir,ext))) {
+      source(file.path(config.dir,ext))
+    } else {
+      flog.error("Can't find extension file %s.", ext)
+    }
+  }
+}
 
 doBuild(sess, EA.config$Tables,config.dir)
--- inst/conf/RunEABN.R
+++ inst/conf/RunEABN.R
@@ -2,9 +2,19 @@ library(R.utils)
 library(EABN)
 library(PNetica)
 
-appStem <- cmdArg("app",NULL)
-if (FALSE) {
-  appStem <- "userControl"
+if (interactive()) {
+  ## Edit these for the local application
+  appStem <- "P4test"
+  loglevel <- ""
+  noprep <- FALSE
+  override <- FALSE
+} else {
+  appStem <- cmdArg("app",NULL)
+  if (is.null(app) || !grepl("^ecd://",app))
+    stop("No app specified, use '--args app=ecd://...'")
+  loglevel <- cmdArg("level","")
+  noprep <- as.logical(cmdArg("noprep",FALSE))
+  override <- as.logical(cmdArg("override",FALSE))
 }
 
 source("/usr/local/share/Proc4/EAini.R")
@@ -43,5 +53,5 @@ for (ext in EA.config$extensions) {
 }
 
 eng <- doRunrun(app,sess,EA.config,EAeng.local,config.dir,outdir,
-                logfile=logfile)
+                logfile=logfile,override=override,noprep=noprep)
 
